6bbb0d851c88640c6a6d8a78dd0ed06f91d52b6a
Jonas <jonas.luft@hcu-hamburg.de>
2020-03-11 13:30:08 +0100

    fixed request to send json

--- main.py
+++ main.py
@@ -11,24 +11,32 @@ def getAll():
         ps = soup.find_all("p")
         for p in ps:
             for s in p.stripped_strings:
-                # print(s)
                 perlisisms.append(s)
 
     return perlisisms
 
 def getRandom(perlisisms):
     random_perlisism = perlisisms[random.randint(0,len(perlisisms))]
-
-    print(random_perlisism)
     return random_perlisism
 
 def send(url, data):
+    data = "Here is your daily programming wisdom:\n\'" + data + "\' _- Alan Perlis_"
+
+    # wrap neatly in json
+    json_payload ="{ \"text\": \"" + data + "\" }"
+
     # send to mattermost
-    requests.post(url, data)
+    response = requests.post(url, json_payload)
+    
+    if not response.status_code == 200:
+        print("could not post to", url)
+        print("Error code", response.status_code)
+    else:
+        print("Successfully posted to ", url, response.status_code)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send a random perlisism to given webhook (e.g. mattermost)')
-    parser.add_argument('--webhook', type=str, help="webhook url to send a perlisism to")
+    parser.add_argument('webhook', type=str, help="webhook url to send a perlisism to")
     args = parser.parse_args()
 
     webhook_url = args.webhook
